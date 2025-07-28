b73ac05c43ba1a9983c8c5467793ac9e1c06a61c
Kevin Nitro <trannguyenthaibinh46@gmail.com>
2025-07-29 01:42:38 +0700

    refactor(script): the check dir exist and increase to 200

--- scripts/generate-schemas.sh
+++ scripts/generate-schemas.sh
@@ -5,37 +5,41 @@ set -euo pipefail
 out="${1:-schemas/}"
 mkdir -p "$out"
 
-tags=$(git ls-remote --refs --tags https://github.com/argoproj/argo-cd.git | cut -d/ -f3 | sort --general-numeric-sort --reverse | sed -n '1,100p')
+tags=$(git ls-remote --refs --tags https://github.com/argoproj/argo-cd.git | cut -d/ -f3 | sort --general-numeric-sort --reverse | sed -n '1,200p')
 bin="docker run -i --rm -v ${out}:/out/schemas ghcr.io/yannh/openapi2jsonschema:latest"
 
 counter=0
 
-for tag in master $tags; do
+function generate() {
+  url=https://raw.githubusercontent.com/argoproj/argo-cd/${tag}/assets/swagger.json
+  # prefix=https://raw.githubusercontent.com/KevinNitroG/argocd-json-schema/main/${out}/${tag}/_definitions.json
+  $bin -o "schemas/${tag}/standalone-strict" --expanded --stand-alone --strict "${url}"
+  $bin -o "schemas/${tag}/standalone-strict" --stand-alone --strict "${url}"
+
+  $bin -o "schemas/${tag}/standalone" --expanded --stand-alone "${url}"
+  $bin -o "schemas/${tag}/standalone" --stand-alone "${url}"
+
+  # NOTE: Who needs local huh?
+  #
+  # $bin -o "schemas/${tag}/local" --expanded "${url}"
+  # $bin -o "schemas/${tag}/local" "${url}"
+
+  # TODO: I don't know what to name this. But it just... doesn't neccessary
+  #
+  # $bin -o "schemas/${tag}" --expanded --kubernetes --prefix "${prefix}" "${url}"
+  # $bin -o "schemas/${tag}" --kubernetes --prefix "${prefix}" "${url}"
+} 1>/dev/null
+
+tag='master'
+
+generate
+
+for tag in $tags; do
   counter=$((counter + 1))
-  if [ -d "${out}/${tags}" ]; then
+  if [ -d "${out}/${tag}" ]; then
     continue
   fi
-
-  url=https://raw.githubusercontent.com/argoproj/argo-cd/${tag}/assets/swagger.json
   printf -- '---\n'
   echo "Order: $counter: Generating for $tag..."
-  # prefix=https://raw.githubusercontent.com/KevinNitroG/argocd-json-schema/main/${out}/${tag}/_definitions.json
-
-  {
-    $bin -o "schemas/${tag}/standalone-strict" --expanded --stand-alone --strict "${url}"
-    $bin -o "schemas/${tag}/standalone-strict" --stand-alone --strict "${url}"
-
-    $bin -o "schemas/${tag}/standalone" --expanded --stand-alone "${url}"
-    $bin -o "schemas/${tag}/standalone" --stand-alone "${url}"
-
-    # NOTE: Who needs local huh?
-    #
-    # $bin -o "schemas/${tag}/local" --expanded "${url}"
-    # $bin -o "schemas/${tag}/local" "${url}"
-
-    # TODO: I don't know what to name this. But it just... doesn't neccessary
-    #
-    # $bin -o "schemas/${tag}" --expanded --kubernetes --prefix "${prefix}" "${url}"
-    # $bin -o "schemas/${tag}" --kubernetes --prefix "${prefix}" "${url}"
-  } 1>/dev/null || true # In case it doesn't exist
+  generate || true
 done
