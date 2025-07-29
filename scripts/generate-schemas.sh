467971394382e73f231bc55dbfcabd2fdfe4036d
Kevin Nitro <trannguyenthaibinh46@gmail.com>
2025-07-29 14:22:25 +0700

    feat(script): parallel 4 process, no suppress error

--- scripts/generate-schemas.sh
+++ scripts/generate-schemas.sh
@@ -6,40 +6,48 @@ out="${1:-schemas/}"
 mkdir -p "$out"
 
 tags=$(git ls-remote --refs --tags https://github.com/argoproj/argo-cd.git | cut -d/ -f3 | sort --general-numeric-sort --reverse | sed -n '1,200p')
-bin="docker run -i --rm -v ${out}:/out/schemas ghcr.io/yannh/openapi2jsonschema:latest"
+
+special_tags=$('main' 'stable')
+
+container_id=$(docker run -d -v "${out}:/out/schemas" ghcr.io/yannh/openapi2jsonschema:latest sleep infinity)
+
+function cleanup() {
+  docker stop "$container_id" >/dev/null 2>&1 || true
+  docker rm "$container_id" >/dev/null 2>&1 || true
+}
+trap cleanup EXIT
 
 counter=0
 
 function generate() {
   url=https://raw.githubusercontent.com/argoproj/argo-cd/${tag}/assets/swagger.json
   # prefix=https://raw.githubusercontent.com/KevinNitroG/argocd-json-schema/main/${out}/${tag}/_definitions.json
-  $bin -o "schemas/${tag}/standalone-strict" --expanded --stand-alone --strict "${url}"
-  $bin -o "schemas/${tag}/standalone-strict" --stand-alone --strict "${url}"
+  docker exec "$container_id" openapi2jsonschema -o "schemas/${tag}/standalone-strict" --expanded --stand-alone --strict "${url}" >/dev/null &
+  docker exec "$container_id" openapi2jsonschema -o "schemas/${tag}/standalone-strict" --stand-alone --strict "${url}" >/dev/null &
+  docker exec "$container_id" openapi2jsonschema -o "schemas/${tag}/standalone" --expanded --stand-alone "${url}" >/dev/null &
+  docker exec "$container_id" openapi2jsonschema -o "schemas/${tag}/standalone" --stand-alone "${url}" >/dev/null &
 
-  $bin -o "schemas/${tag}/standalone" --expanded --stand-alone "${url}"
-  $bin -o "schemas/${tag}/standalone" --stand-alone "${url}"
+  wait
 
   # NOTE: Who needs local huh?
   #
-  # $bin -o "schemas/${tag}/local" --expanded "${url}"
-  # $bin -o "schemas/${tag}/local" "${url}"
+  # docker exec "$container_id" openapi2jsonschema -o "schemas/${tag}/local" --expanded "${url}" &
+  # docker exec "$container_id" openapi2jsonschema -o "schemas/${tag}/local" "${url}" &
 
   # TODO: I don't know what to name this. But it just... doesn't neccessary
   #
-  # $bin -o "schemas/${tag}" --expanded --kubernetes --prefix "${prefix}" "${url}"
-  # $bin -o "schemas/${tag}" --kubernetes --prefix "${prefix}" "${url}"
-} 1>/dev/null
+  # docker exec "$container_id" openapi2jsonschema -o "schemas/${tag}" --expanded --kubernetes --prefix "${prefix}" "${url}" &
+  # docker exec "$container_id" openapi2jsonschema -o "schemas/${tag}" --kubernetes --prefix "${prefix}" "${url}" &
+  wait
+}
 
-tag='master'
-
-generate
-
-for tag in $tags; do
+for tag in "${special_tags[@]}" "${tags[@]}"; do
   counter=$((counter + 1))
-  if [ -d "${out}/${tag}" ]; then
+  if [[ ${special_tags[*]} =~ $tag && -d "${out}/${tag}" ]]; then
+    echo "Order: $counter: Skip generating for $tag"
     continue
   fi
   printf -- '---\n'
   echo "Order: $counter: Generating for $tag..."
-  generate || true
+  generate
 done
